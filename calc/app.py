0b80bf37b99a1a5309fef1bb35a2693d8f2eacdc
Phil Bailey <philbailey@msn.com>
2020-12-15 10:09:32 -0500

    calc complete

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,37 @@
 # Put your app in here.
+from operations import add, sub, mult, div
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/add")
+def route_add():
+    """add two number using helper method"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(add(a, b))
+
+
+@app.route("/sub")
+def route_sub():
+    """subtract two number using helper method"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(sub(a, b))
+
+
+@app.route("/mult")
+def route_mult():
+    """multiply two number using helper method"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(mult(a, b))
+
+
+@app.route("/div")
+def route_div():
+    """divide two number using helper method"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return str(div(a, b))
